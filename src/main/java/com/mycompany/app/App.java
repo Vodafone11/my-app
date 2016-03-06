25e23cdf97fd19c77ea077e7a96aba30aa79f5ad
root <shashi.singh.650@gmail.com>
2016-03-07 01:45:14 +0530

    scm poll commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "shahsi Hello World!" );
+        System.out.println( "shahsi Hello World!chages for poll" );
     }
 }
